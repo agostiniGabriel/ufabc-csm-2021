d7b4b43a489fb0f9844e3c22e21ced747e4975bc
agostiniGabriel <agostini_gabriel@hotmail.com>
2021-09-30 22:48:26 -0300

    feat: :sparkles: Navegação Lab2

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ document.getElementById("lab1").addEventListener("click", function(){
     navigate("lab1");
 });
 
+document.getElementById("lab2").addEventListener("click", function(){
+    navigate("lab2");
+});
+
 function navigate(ref){
     console.log("@@@ Navegando para " + ref);
     window.location.assign('pages/'+ ref +'/' + ref + ".html");
