e554cbbbcea311e8b322ccc833542cc8f1d53eac
agostiniGabriel <agostini_gabriel@hotmail.com>
2021-10-15 20:47:32 -0300

    fix Navegação lab4

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ document.getElementById("lab3").addEventListener("click", function(){
     navigate("lab3");
 });
 
+document.getElementById("lab4").addEventListener("click", function(){
+    navigate("lab4");
+});
+
 function navigate(ref){
     console.log("@@@ Navegando para " + ref);
     window.location.assign('pages/'+ ref +'/' + ref + ".html");
