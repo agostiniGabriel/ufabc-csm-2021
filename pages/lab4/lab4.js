2a2689d2ab1c24fb5ec557a49913f1fb104d5246
agostiniGabriel <agostini_gabriel@hotmail.com>
2021-10-15 22:22:42 -0300

    feat: :sparkles: finalização lab4

--- pages/lab4/lab4.js
+++ pages/lab4/lab4.js
@@ -1,14 +1,21 @@
 
 var backBTN = document.getElementById("btnVoltar");
-var colabBTN = document.getElementById('btnColab');
+var colabBTN1 = document.getElementById('btnColab1');
+var colabBTN2 = document.getElementById('btnColab2');
 
 backBTN.addEventListener("click",function(){
     returnHome();
 });
 
-colabBTN.addEventListener('click',function(){
-    window.location.assign('https://colab.research.google.com/drive/1rAIHQuJ-4AylbzyrfjYZGdFCeph5G_tE#scrollTo=tnMyvbgoeYqH')
+colabBTN1.addEventListener('click',function(){
+    window.location.assign('https://colab.research.google.com/drive/12LCoU4Lli41FHcyf6w1F3ry0SdnxphqN?usp=sharing')
 });
+
+
+colabBTN2.addEventListener('click',function(){
+    window.location.assign('https://colab.research.google.com/drive/1M5AxLI2u0_XtZpU8w5kH3YL7ugripJ0h?usp=sharing')
+});
+
 
 function returnHome(){
     let url = window.location.href.split("pages");
