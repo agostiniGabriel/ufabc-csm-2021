adc8af0c94fbcabb57246bd59317b975201ae3c8
agostiniGabriel <agostini_gabriel@hotmail.com>
2021-12-10 18:20:33 -0300

    add lab8

--- index.js
+++ index.js
@@ -27,6 +27,10 @@ document.getElementById("lab7").addEventListener("click", function(){
 });
 
 
+document.getElementById("lab8").addEventListener("click", function(){
+    navigate("lab8");
+});
+
 function navigate(ref){
     console.log("@@@ Navegando para " + ref);
     window.location.assign('pages/'+ ref +'/' + ref + ".html");
